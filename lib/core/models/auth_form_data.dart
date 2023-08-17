2f38355fe333467ae120ff47461ccca8320f64ac
EMelo26 <eltonmelo23@gmail.com>
2023-08-17 17:42:53 -0300

    Firebase Login

---
+++ lib/core/models/auth_form_data.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+enum AuthMode { signup, login }
+
+class AuthFormData {
+  String name = '';
+  String email = '';
+  String Endereco = '';
+  String Numero = '';
+  String Complemento = '';
+  String password = '';
+  File? image;
+  AuthMode _mode = AuthMode.login;
+
+  bool get isLogin {
+    return _mode == AuthMode.login;
+  }
+
+  bool get isSignup {
+    return _mode == AuthMode.signup;
+  }
+
+  void toggleAuthMode() {
+    _mode = isLogin ? AuthMode.signup : AuthMode.login;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:chat/core/services/notification/chat_notification_service.dart';
+import 'package:chat/pages/auth_or_app_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const Cadastro_Cliente());
+}
+
+class Cadastro_Cliente extends StatelessWidget {
+  const Cadastro_Cliente({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ChatNotificationService(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Cadastro Cliente',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const AuthOrAppPage(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
